d848d0e6cf8014b985195034d4b5372e84b290c5
Yaraslau Lazakovich <ylazakovich@gmail.com>
2026-04-30 18:07:36 +0200

    fix(frontend): listen on Railway PORT env var instead of hardcoded 80

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
-envsubst '${BACKEND_URL}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '${BACKEND_URL} ${PORT}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
 exec nginx -g 'daemon off;'
